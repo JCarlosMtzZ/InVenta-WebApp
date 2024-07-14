15a22724a044738937467e7e9e026ec78a111aef
JCarlosMtzZ <juancmtz777@gmail.com>
2024-07-13 19:57:57 -0600

    Installed TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="w-[100vw] h-[100vh] bg-gray-100 flex justify-center items-center ">
+      <h1 className="text-6xl font-bold text-blue-500">Hello, TailwindCSS</h1>
+    </div>
+  )
+}
+
+export default App
